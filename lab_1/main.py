d5d60cb1de8d4c15ab98b57f862704a1f63196c8
vladementei@gmail.com <Pravda1650>
2020-09-05 18:46:06 +0300

    lab_1 vigenere encryption

--- lab_1/main.py
+++ lab_1/main.py
@@ -12,15 +12,53 @@ def write_file(path, content):
     file.close()
 
 
-def encrypt_cesar(array, shift):
+def is_letter(char):
+    return 'a' <= char <= 'z' or 'A' <= char <= 'Z'
+
+
+def is_capital(letter):
+    return 'A' <= letter <= 'Z'
+
+
+def normalize_size(letter, is_caps):
+    basis = 'A' if is_caps else 'a'
+    return chr(((ord(letter) - ord(basis)) % 26) + ord(basis))
+
+
+def vigenere_shift(letter, is_caps):
+    return ord(letter) - ord('A' if is_caps else 'a')
+
+
+def cesar(array, shift, enc=True):
     for i in range(len(array)):
-        array[i] = chr((ord(array[i]) + shift) % 256)
+        if is_letter(array[i]):
+            is_caps = is_capital(array[i])
+            array[i] = normalize_size(chr(ord(array[i]) + (shift if enc else -shift)), is_caps)
+    return array
+
+
+def vigenere(array, key, enc=True):
+    key_length = len(key)
+    key_list = list(map(lambda x: {'chr': x, 'is_capital': is_capital(x)}, key))
+    for i in range(len(array)):
+        if is_letter(array[i]):
+            is_caps = is_capital(array[i])
+            shift = vigenere_shift(key_list[i % key_length]['chr'], key_list[i % key_length]['is_capital'])
+            array[i] = normalize_size(chr(ord(array[i]) + (shift if enc else -shift)), is_caps)
     return array
 
 
 input_characters = read_file('resources/input1.txt')
-encrypted = encrypt_cesar(input_characters, 3)
-print(encrypted)
-decrypted = encrypt_cesar(encrypted, -3)
 
-write_file('resources/output1.txt', decrypted)
+# vigenere
+# encrypted = vigenere(input_characters, 'moUsE')
+# print(encrypted)
+# decrypted = vigenere(encrypted, 'moUsE', False)
+# print(decrypted)
+
+# cesar
+encrypted = cesar(input_characters, 3)
+print(encrypted)
+decrypted = cesar(encrypted, 3, False)
+print(decrypted)
+# write_file('resources/output1.txt', decrypted)
