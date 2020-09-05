da8cbb110470999cf29048142f843205518bde7a
vladementei@gmail.com <Pravda1650>
2020-09-05 20:22:17 +0300

    lab_1 casisci algorithm

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,3 +1,14 @@
+import math
+import re
+from functools import reduce
+from collections import Counter
+
+
+def find_gcd(array):
+    x = reduce(math.gcd, array)
+    return x
+
+
 def read_file(path):
     file = open(path, 'r+')
     characters = [elem for elem in file.read()]
@@ -58,17 +69,42 @@ def vigenere(array, key, enc=True):
     return answer
 
 
+def casisci(text):
+    text_length = len(text)
+    text = text.lower()
+    gsd_array = []
+    for l in range(2, 10):
+        for i in range(text_length - l + 1):
+            search = text[i: i + l]
+            positions = [token.start() for token in re.finditer(search, text)]
+            if len(positions) > 1:
+                distances = []
+                for j in range(1, len(positions)):
+                    distances.append(positions[j] - positions[j - 1])
+                gcd = find_gcd(distances)
+                if gcd > 1:
+                    gsd_array.append(gcd)
+                    # print(l, ' ', search, ' ', positions, ' ', distances, ' ', gcd)
+    # print(sorted(gsd_array, key=gsd_array.count, reverse=True))
+    # print(Counter(gsd_array))
+    counter = Counter(gsd_array)
+    answer = next(iter(counter))
+    print('probability = ', counter[answer] / len(gsd_array))
+    return answer
+
+
 input_characters = read_file('resources/input1.txt')
 
 # vigenere
-encrypted = vigenere(input_characters, 'moUsE')
-print(encrypted)
-decrypted = vigenere(encrypted, 'moUsE', False)
-print(decrypted)
+encrypted = vigenere(input_characters, 'mouse')
+# print(encrypted)
+print('key length = ', casisci("".join(encrypted)))
+# decrypted = vigenere(encrypted, 'moUsE', False)
+# print(decrypted)
 
 # cesar
 encrypted = cesar(input_characters, 3)
-print(encrypted)
-decrypted = cesar(encrypted, 3, False)
-print(decrypted)
+# print(encrypted)
+# decrypted = cesar(encrypted, 3, False)
+# print(decrypted)
 # write_file('resources/output1.txt', decrypted)
